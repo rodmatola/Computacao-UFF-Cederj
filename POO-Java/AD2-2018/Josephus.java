04ec2e74ef3a431149878ec179e5b2c9d148452a
Rodrigo Matola <rodmatola@gmail.com>
2018-05-14 15:12:09 -0300

    Versão final

--- POO-Java/AD2-2018/Josephus.java
+++ POO-Java/AD2-2018/Josephus.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Josephus {
@@ -5,65 +11,57 @@ public class Josephus {
 	public static void main(String[] args) {
 		
 		ArrayList<String> nomesOriginal = new ArrayList<>(); 
-//		nomesOriginal.add("1");
-//		nomesOriginal.add("2");
-//		nomesOriginal.add("3");
-//		nomesOriginal.add("4");
-//		nomesOriginal.add("5");
-
-//		nomesOriginal.add("José");
-//		nomesOriginal.add("Henrique");
-//		nomesOriginal.add("Francisco");
-//		nomesOriginal.add("Manoel");
-//		nomesOriginal.add("Rafael");
-//		nomesOriginal.add("Marcus");
 		
-//		nomesOriginal.add("A");
-//		nomesOriginal.add("J");
-//		nomesOriginal.add("5");
-//		nomesOriginal.add("P");
-//		nomesOriginal.add("Q");
-		
-		nomesOriginal.add("Mario");
-		nomesOriginal.add("Carlos");
-		nomesOriginal.add("Antonio");
-		nomesOriginal.add("Pedro");
-		nomesOriginal.add("Joaquim");		
-		nomesOriginal.add("Mauro");
-		nomesOriginal.add("Roberto");
-		nomesOriginal.add("Joao");
-		nomesOriginal.add("Paulo");
-		
-		int quantidade_de_ks = 1;
-		
+		BufferedReader br;
+		int quantidadeDeKs = 0;
 		ArrayList<Integer> k = new ArrayList<>();
-		k.add(3);
-		k.add(4);
-		k.add(7);
 		
+		try {
+			br = new BufferedReader(new FileReader(args[0]));
+			String linha = br.readLine();
+			while (!linha.trim().equals("FIM")) {
+				nomesOriginal.add(linha);
+				linha =  br.readLine();
+			}
+			
+			quantidadeDeKs = Integer.parseInt(br.readLine().trim());
+			
+			
+			for (int i = 0; i < quantidadeDeKs; i++) {
+				k.add(Integer.parseInt(br.readLine()));
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		for (int i = 0; i < quantidade_de_ks; i++) {
+		for (int i = 0; i < quantidadeDeKs; i++) {
 			int position = k.get(i) - 1;
 			ArrayList<String> nomes = new ArrayList<>(nomesOriginal);
 			int tam = nomes.size(); 
-			System.out.println(nomes);
 			if (position >= tam - 1) {
 				position = position % (tam - 1) - 1;
 			}
 			while ( tam > 1 ) {
-				//System.out.println(nomes.size());
-				
 				nomes.remove(position);
-				System.out.println(nomes);
 				position = position - 1 + k.get(i);
 				if (position >= tam - 1) {
 					position = position % (tam - 1);
 				}
 				tam = nomes.size();
 			}
-			System.out.println("---------------");
-			System.out.println(k.get(i) + " " + nomes.get(0));
-			System.out.println("---------------");
+			String saida = k.get(i) + " " + nomes.get(0);
+			System.out.println(saida);
+			try {
+				Files.write(Paths.get("saida-" + args[0]),saida.getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	
 
